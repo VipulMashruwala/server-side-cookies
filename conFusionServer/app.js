eed4f2c4cc22aa7f17a8cc3050373e861bc76859
Vipul Mashruwala <mashruwala.vipu@gmail.com>
2021-07-01 13:40:59 +0530

    express session

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,8 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,16 +12,16 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
-const  mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
-mongoose.connect('mongodb://localhost:27017/conFusion',{ 
+mongoose.connect('mongodb://localhost:27017/conFusion', {
   useNewUrlParser: true,
-  useUnifiedTopology: true 
+  useUnifiedTopology: true
 }).then(db => {
-    console.log('connected correctly to server');
+  console.log('connected correctly to server');
 })
-.catch(err => console.log(err));
+  .catch(err => console.log(err));
 
 var app = express();
 
@@ -30,64 +32,52 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
-const auth = ((req, res, next) =>{
-    console.log(req.signedCookies);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-    if(!req.signedCookies.user){
-        var authHeader = req.headers.authorization;
-        if (!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-            return;
-        }
-    
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        var user = auth[0];
-        var pass = auth[1];
-        if (user == 'admin' && pass == 'password') {
-            res.cookie('user','admin',{signed: true})
-            next(); // authorized
-        } 
-        else {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');      
-            err.status = 401;
-            next(err);
-        }
+function auth(req, res, next) {
+  console.log(req.session);
+
+  if (!req.session.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
     }
-    else{
-      if(req.signedCookies.user === 'admin'){
-        next();
-      }
-      else{
-        var err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-      }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
     }
-
-    
-})
+  }
+}
 
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
